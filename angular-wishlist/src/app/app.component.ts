fd88188dac5d939c531199c05d87f0668a8bb5e5
Luis Miguel Oviedo L <mimi1296@javerianacali.edu.co>
2020-12-21 14:25:03 -0500

    Final Angular Travel Wishlist

--- angular-wishlist/src/app/app.component.ts
+++ angular-wishlist/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-wishlist';
+  title = 'Angular Travel Wishlist';
   agregar(titulo: HTMLInputElement)
   {
     this.title = titulo.value;
--- angular-wishlist/src/app/lista-destino/lista-destino.component.ts
+++ angular-wishlist/src/app/lista-destino/lista-destino.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DestinoViaje } from '../models/destino-viaje.models';
 
 @Component({
   selector: 'app-lista-destino',
@@ -6,12 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-destino.component.css']
 })
 export class ListaDestinoComponent implements OnInit {
-  destinos: string[];
+  destinos: DestinoViaje[];
   constructor() { 
-    this.destinos = ['Aruba', 'Lima', 'Buenos Aires'];
+    this.destinos = [];
   }
 
   ngOnInit(): void {
+  }
+
+  guardar(nombre: string, url: string): boolean{
+    this.destinos.push(new DestinoViaje(nombre, url))
+    console.log(this.destinos);
+    return false;
   }
 
 }
